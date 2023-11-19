de59d85aaad568978bcf41edb656d6e61f160c2e
Geydi <Geydi@192.168.1.137>
2023-11-19 11:23:47 +0100

    Proyecto 1

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,16 @@
+//este primer proyecto es muy sencillo solo ejecutaremos un println y se nos muestra en la consola el resultado
+
+package javaapplication1;
+
+/**
+ * @author Geydi
+ */
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        // TO-DO code application logic here
+     System.out.println("Bienvenida a este mundo de la programacion");
+         
+    }
+    
+}
